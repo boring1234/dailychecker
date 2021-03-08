07d7f9dbf9095a8060c891dc208711d82ca590e8
zebin chen <73151388+boring1234@users.noreply.github.com>
2021-03-08 09:50:33 -0800

    Add files via upload

---
+++ docs/init_firebase.js
@@ -0,0 +1,21 @@
+// var firebaseConfig = {
+//     apiKey: "AIzaSyB8jBV9C25DNYxLkai2V88cqXr6oBXbZWw",
+//     authDomain: "covid-19-fba01.firebaseapp.com",
+//     projectId: "covid-19-fba01",
+//     storageBucket: "covid-19-fba01.appspot.com",
+//     messagingSenderId: "761171749857",
+//     appId: "1:761171749857:web:d54200aeabdcc38328c0a0"
+// };
+// firebase.initializeApp(firebaseConfig);
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCa73R02XR0nPwdxsTNPl_DYu4IOoE2k5U",
+    authDomain: "inf172project.firebaseapp.com",
+    databaseURL: "https://inf172project-default-rtdb.firebaseio.com",
+    projectId: "inf172project",
+    storageBucket: "inf172project.appspot.com",
+    messagingSenderId: "754889398783",
+    appId: "1:754889398783:web:54d45bb7705e76ab6d6fca"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
